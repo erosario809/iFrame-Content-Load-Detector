e9eb866269b3626bbe9b3f98145dcaeb3512d82f
Edwin Rosario <erosario@webmd.net>
2018-02-23 13:11:34 -0500

    updated console.logs for better understanding

--- src/iframeContentLoadDetector.js
+++ src/iframeContentLoadDetector.js
@@ -19,35 +19,33 @@ window.onload = function (){
 
     document.body.appendChild(iframe);
 
-	try{
+	//in Firefox the contentDocument.URL will return a string 'about:blank' if no content was loaded but will fail otherwise indicating that it loaded successfully in firefox...for all other browser we'll use a setTimeout to detect if content successfully loaded
+	if(browser == 'Firefox'){
 
-		//in Firefox the contentDocument.URL will return a string 'about:blank' if no content was loaded but will fail otherwise indicating that it loaded successfully in firefox...for all other browser we'll use a setTimeout to detect if content successfully loaded
-		if(browser == 'Firefox'){
+		try{
 			iframe.onload = function(){
-				console.log(document.getElementById('mainIframe').contentDocument.URL);
 				var check = document.getElementById('mainIframe').contentDocument.URL;
 				if(check == 'about:blank'){
 					//iframe loaded but without content...fire off your process here
+					console.log("UNABLE TO LOAD IFRAME CONTENT");
 					alert("whohooo iframe didn't load and i detected it");
 				}
 			};
-		}else{
-			iframe.onload = function(){
-				if(timepast) {
-		            console.log("Able to iframe and it's content");
-		        }
-		        else {
-		            //unable to load content in iframe...fire off your process here
-		            console.log("UNABLE TO LOAD IFRAME CONTENT");
-		            alert("whohooo iframe didn't load and i detected it");
-		        }
-			}
+		}catch(e){
+			console.log("iframe loaded successfully");
 		}
-			
-
-	}catch(e){
-		//neither detection method worked....someting went wrong upon creating and appending the iframe
-		console.log(e);
+		
+	}else{
+		iframe.onload = function(){
+			if(timepast) {
+	            console.log("iframe loaded successfully");
+	        }
+	        else {
+	            //unable to load content in iframe...fire off your process here
+	            console.log("UNABLE TO LOAD IFRAME CONTENT");
+	            alert("whohooo iframe didn't load and i detected it");
+	        }
+		}
 	}
 	
 }
